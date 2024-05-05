9cfc4793532c78b433aca762f5070b4799305987
rzsamrio <samuelekop144@gmail.com>
2024-05-05 13:59:24 +0100

    changes

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,12 @@
 # Installing a package using puppet
 
+package { 'Werkzeug':
+  ensure  => '2.1.1',
+  provider => 'pip3'
+}
+
+
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip',
+  provider => 'pip3',
 }
