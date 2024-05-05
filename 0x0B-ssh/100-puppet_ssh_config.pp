8460a1666ef5b451246c0cb834f97e530b800f9e
rzsamrio <samuelekop144@gmail.com>
2024-05-06 00:28:57 +0100

    [0x0B] on replace

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,15 +3,17 @@
 include stdlib
 
 file_line { 'Turn off passwd auth' :
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PubkeyAuthentication yes',
-  match  => '^PubkeyAuthentication'
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PubkeyAuthentication yes',
+  match   => '^PubkeyAuthentication',
+  replace => 'true'
 }
 
 file_line {'Declare identity file' :
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school',
-  match  => '^IdentityFile'
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/school',
+  match   => '^IdentityFile',
+  replace => 'true'
 }
