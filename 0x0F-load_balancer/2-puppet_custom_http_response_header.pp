bbe1aeb458c7ab741ddde345741cd59cd1649e79
rzsamrio <samuelekop144@gmail.com>
2024-05-15 04:43:07 +0100

    [0x0F] puppet?

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,50 @@
+# Configure server with http header
+
+# Install nginx server
+package { 'nginx' :
+    ensure   => installed,
+    provider => 'apt',
+}
+
+# Get rid of all html files
+file { '/var/www/html' :
+    ensure  => 'directory',
+    purge   => true,
+    recurse => true,
+}
+
+# Create landing page 
+file {'/var/www/html/index.html' :
+    ensure  => present,
+    content => 'Hello World!'
+}
+
+
+# Create the config file
+file { '/etc/nginx/sites-available/default':
+    ensure  => present,
+    content => 'server {
+    add_header X-Served-By \"$(hostname)\";
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html; 
+    
+    server_name _;
+
+    location / {
+        try_files $uri $uri/ =404;
+    }
+
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    }
+}'
+}
+
+# Ensure nginx is running
+service { 'nginx' :
+    ensure => running,
+    name   => 'nginx'
+}
