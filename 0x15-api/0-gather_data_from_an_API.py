d01cd5675f21e53945a18977d28116060d837bb9
rzsamrio <samuelekop144@gmail.com>
2024-06-04 04:03:22 +0100

    [0X15] Add task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Gather data from a REST API """
+import sys
+import requests
+
+if __name__ == '__main__':
+    id = int(sys.argv[1])
+    uri = 'https://jsonplaceholder.typicode.com'
+    user = requests.get(uri + '/users/' + sys.argv[1]).json()
+    Name = user['name']
+
+    tasks = requests.get(uri + '/todos?userId=' + sys.argv[1]).json()
+    Total = len(tasks)
+    done_tasks = requests.get(uri + '/todos?userId=' + sys.argv[1] +
+                              '&completed=true').json()
+    Done = len(done_tasks)
+
+    print(f'Employee {Name} is done with tasks({Done}/{Total}):')
+    for task in done_tasks:
+        print(f'\t {task["title"]}')
