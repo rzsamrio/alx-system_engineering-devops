23f9d16fc3ad5d9b7d7075d8af03af582a074b86
rzsamrio <samuelekop144@gmail.com>
2024-06-04 13:19:38 +0100

    [0x15] add task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -5,12 +5,15 @@ import requests
 import sys
 
 if __name__ == '__main__':
-	user = (requests.get('https://jsonplaceholder.typicode.com/users/' + sys.argv[1])).json()
-	Name = user['username']
-	
-	tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]).json()
-	with open(f'{sys.argv[1]}.csv', 'w', newline='') as csvfile:
-		writer = csv.writer(csvfile)
-		for task in tasks:
-			row = [str(sys.argv[1]), str(Name), str(task['completed']), str(task['title'])]
-			writer.writerow(row)
+    user = (requests.get('https://jsonplaceholder.typicode.com/users/'
+            + sys.argv[1])).json()
+    Name = user['username']
+
+    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId='
+                         + sys.argv[1]).json()
+    with open(f'{sys.argv[1]}.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            row = [str(sys.argv[1]), str(Name), str(task['completed']),
+                   str(task['title'])]
+            writer.writerow(row)
