ad791c64cc7159852035142ec1b26394ded09e6d
rzsamrio <samuelekop144@gmail.com>
2024-06-04 12:53:52 +0100

    [0x15] Add task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" """
+""" Export some data to CSV """
 import csv
 import requests
 import sys
 
 if __name__ == '__main__':
+	user = (requests.get('https://jsonplaceholder.typicode.com/users/' + sys.argv[1])).json()
+	Name = user['username']
+	
 	tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + sys.argv[1]).json()
 	with open(f'{sys.argv[1]}.csv', 'w', newline='') as csvfile:
 		writer = csv.writer(csvfile)
 		for task in tasks:
-			writer.writerow(task.values())
+			row = [str(sys.argv[1]), str(Name), str(task['completed']), str(task['title'])]
+			writer.writerow(row)
