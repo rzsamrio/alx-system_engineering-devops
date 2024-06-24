d8f583b36387075c9a827e13f91293a57f6f0b4a
rzsamrio <samuelekop144@gmail.com>
2024-06-24 20:44:09 +0100

    [0x16] arr to .get

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,11 +6,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     uri = 'https://www.reddit.com/r/' + subreddit + '/about.json'
-    head = {'user-agent': 'PC-EKSCM'}
+    head = {'user-agent': 'PC_ALX'}
     try:
         response = requests.get(uri, headers=head, allow_redirects=False)
-        info = (response.json())['data']
-        a = info['subscribers']
+        info = (response.json()).get('data')
+        a = info.get('subscribers')
         return(a)
     except Exception:
         return (0)
