84ac79e6773456cd9a08427849c6e8093390391b
rzsamrio <samuelekop144@gmail.com>
2024-06-25 06:00:04 +0100

    requests to urllib

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """ Return the total number of subscribers on a subreddit"""
-import requests
+import urllib.request
+import json
 
 
 def number_of_subscribers(subreddit):
     uri = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    head = {'user-agent': 'PC_ALX'}
+    head = {'User-Agent': 'PC_ALX'}
+    request = urllib.request.Request(uri, headers=head)
+
     try:
-        response = requests.get(uri, headers=head, allow_redirects=False)
-        info = (response.json()).get('data')
-        val = info.get('subscribers')
-        return (val)
+        with urllib.request.urlopen(request) as response:
+            data = response.read().decode('utf-8')
+            info = json.loads(data).get('data')
+            val = info.get('subscribers')
+            return val
     except Exception:
-        return (0)
+        return 0
